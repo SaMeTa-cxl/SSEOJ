c76b648cad81a46ab869aed12387142747865905
SaMeTa-cxl <chenxlei23@mail2.sysu2.edu.cn>
2024-11-07 16:48:14 +0800

    给用户模型添加了简介字段和头像字段，添加和配置了静态文件夹

--- account/models.py
+++ account/models.py
@@ -13,6 +13,8 @@ class User(AbstractUser):
     username = models.CharField(max_length=20, unique=True)
     email = models.EmailField(null=True, blank=True)
     create_time = models.DateTimeField(auto_now_add=True, null=True)
+    profile = models.TextField(null=True, blank=True)
+    avatar = models.CharField(max_length=50, default='static/avatar.png/')
     # 用户类型：Normal/Admin
     user_type = models.CharField(max_length=20, default=UserType.NORMAL_USER)
 
