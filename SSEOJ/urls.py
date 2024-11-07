22fda1f030cbf4b612c16c25943fd7b12fed9399
SaMeTa-cxl <chenxlei23@mail2.sysu2.edu.cn>
2024-11-07 22:04:08 +0800

    test git

--- SSEOJ/urls.py
+++ SSEOJ/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     # test github
+    # test github2
     path("admin/", admin.site.urls),
     path("api/", include("account.urls.oj")),
     path("api/", include("problem.urls.oj")),
