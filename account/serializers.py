2ff2e3d74df809c519c8ea3271b58267667740d8
SaMeTa-cxl <chenxlei23@mail2.sysu2.edu.cn>
2024-11-14 20:19:54 +0800

    把UserSerializer转移到account中

--- account/serializers.py
+++ account/serializers.py
@@ -1,6 +1,21 @@
 from rest_framework import serializers
 
+from account.models import User
+
 
 class UserLoginSerializer(serializers.Serializer):
     email = serializers.EmailField()
     password = serializers.CharField()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        needed_fields = kwargs.pop('needed_fields', None)
+        super(UserSerializer, self).__init__(*args, **kwargs)
+        if needed_fields:
+            for field in set(self.fields.keys()) - set(needed_fields):
+                self.fields.pop(field)
