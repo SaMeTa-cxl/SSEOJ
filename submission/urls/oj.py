516201eef38d17c1e3fd117acc12b730ec9741e5
SaMeTa-cxl <chenxlei23@mail2.sysu2.edu.cn>
2024-12-06 16:07:30 +0800

    搭建好判题和提交记录的框架

---
+++ submission/urls/oj.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from ..views.oj import *
+
+urlpatterns = [
+    path("problem/submit/", ProblemSubmitAPI.as_view(), name="problem_submit"),
+    path("problem/<int: problem_id>/submissions/", ProblemSubmissionsAPI.as_view(), name="problem_submissions")
+]
---
+++ submission/views/oj.py
@@ -0,0 +1,11 @@
+from rest_framework.views import APIView
+
+
+class ProblemSubmissionsAPI(APIView):
+    def get(self, request, problem_id):
+        pass
+
+
+class ProblemSubmitAPI(APIView):
+    def post(self, request):
+        pass
