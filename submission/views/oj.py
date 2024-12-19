8c42e347ff5daffda1819e1919b4b5c8512a84e2
王心怡正牌男友 <ty2434519917@foxmail.com>
2024-12-19 21:38:33 +0800

    ProblemSubmissionAPI demo

--- submission/views/oj.py
+++ submission/views/oj.py
@@ -7,8 +7,32 @@ from judge.tasks import judge_task
 
 class ProblemSubmissionsAPI(APIView):
     def get(self, request, problem_id):
-        pass
+        status = request.query_params.get('status')
+        language = request.query_params.get('language')
+        sort_type = request.query_params.get('sort_type')
 
+        filter_conditions = {'problem_id': problem_id}
+        if status:
+            filter_conditions['status'] = status
+        if language:
+            filter_conditions['language'] = language
+
+        submissions = Submission.objects.filter(**filter_conditions)
+        if sort_type:
+            submissions = submissions.order_by(sort_type)
+
+        submission_data = []
+        for submission in submissions:
+            tmp_dict = {}
+            tmp_dict['submit_time'] = submission.created_time
+            tmp_dict['code'] = submission.code
+            tmp_dict['language'] = submission.language
+            tmp_dict['result'] = submission.result
+            tmp_dict['error_info'] = submission.error_info
+            tmp_dict['time_spent'] = submission.time_spent
+            tmp_dict['memory_spent'] = submission.memory_spent
+            submission_data.append(tmp_dict)
+        return success(submission_data)
 
 class ProblemSubmitAPI(APIView):
     def post(self, request):
