92c8b697540fda4f5b590801584f162df139ea3d
SaMeTa-cxl <chenxlei23@mail2.sysu2.edu.cn>
2024-12-23 14:02:12 +0800

    把所有数据模型添加进了管理后台，现在可以通过django的管理平台来管理数据。

--- conf/admin.py
+++ conf/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from conf.models import SysConfigs
+
 # Register your models here.
+admin.site.register(SysConfigs)
--- problem/admin.py
+++ problem/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from problem.models import Problem, ProblemList, Solution, SolutionComment, StudyPlan, Tag
+
 # Register your models here.
+admin.site.register(Problem)
+admin.site.register(ProblemList)
+admin.site.register(Solution)
+admin.site.register(SolutionComment)
+admin.site.register(StudyPlan)
+admin.site.register(Tag)
