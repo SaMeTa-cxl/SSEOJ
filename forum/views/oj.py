9692203feb85ba3a00e702674ea9ddc1fd562144
王心怡正牌男友 <ty2434519917@foxmail.com>
2024-12-30 21:06:00 +0800

    add under_comment_id to postComment model and fix postNew api

--- forum/views/oj.py
+++ forum/views/oj.py
@@ -99,6 +99,7 @@ class PostCommentNewAPI(APIView):
         comment_content = request.data.get('comment_content')
         user_id = request.user.id
         reply_to_user_id = request.data.get('reply_to_user_id', None)
+        under_comment_id = request.data.get('under_comment_id', None)
 
         if not post_id or not comment_content or not user_id:
             return fail("评论出问题啦！")
@@ -113,6 +114,7 @@ class PostCommentNewAPI(APIView):
                     create_user=user,
                     content=comment_content,
                     reply_to_user=reply_to_user,
+                    under_comment_id=under_comment_id,
                 )
             else:
                 comment = PostComment.objects.create(
@@ -121,6 +123,7 @@ class PostCommentNewAPI(APIView):
                     create_user=user,
                     content=comment_content,
                     reply_to_user=reply_to_user,
+                    under_comment_id=under_comment_id,
                 )
         else:
             if not comment_id:
