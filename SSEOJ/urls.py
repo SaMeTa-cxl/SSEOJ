2f9d2d1f9109cf1c8f13ebf543731e4b0b78b646
inu277 <2394160277@qq.com>
2024-12-30 22:07:28 +0800

    修改时区

--- SSEOJ/urls.py
+++ SSEOJ/urls.py
@@ -24,5 +24,4 @@ urlpatterns = [
     path("api/", include("account.urls.oj")),
     path("api/", include("problem.urls.oj")),
     path("api/", include("forum.urls.oj")),
-    #path("api/", include("submission.urls.oj")),
 ]
