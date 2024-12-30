c9710a82eafd05474b6b17aa9600309c7ac885fe
SaMeTa-cxl <chenxlei23@mail2.sysu2.edu.cn>
2024-12-30 22:06:02 +0800

    update requirements.txt

--- submission/urls/oj.py
+++ submission/urls/oj.py
@@ -4,5 +4,5 @@ from ..views.oj import *
 
 urlpatterns = [
     path("problem/submit/", ProblemSubmitAPI.as_view(), name="problem_submit"),
-    path("problem/<int: problem_id>/submissions/", ProblemSubmissionsAPI.as_view(), name="problem_submissions")
+    path("problem/<int:problem_id>/submissions/", ProblemSubmissionsAPI.as_view(), name="problem_submissions")
 ]
