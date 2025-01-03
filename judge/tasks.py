c663c165e864184f4d71a5f0046d01832e634d5e
SaMeTa-cxl <chenxlei23@mail2.sysu2.edu.cn>
2025-01-03 09:56:27 +0800

    modify setting and config dramatiq

--- judge/tasks.py
+++ judge/tasks.py
@@ -1,6 +1,5 @@
 import dramatiq
 
-from account.models import User
 from submission.models import Submission
 from judge.dispatcher import JudgeDispatcher
 
@@ -8,6 +7,11 @@ from judge.dispatcher import JudgeDispatcher
 @dramatiq.actor(time_limit=3600_000, max_retries=0, max_age=7200_000)
 def judge_task(submission_id, problem_id):
     uid = Submission.objects.get(id=submission_id).user_id
-    if User.objects.get(id=uid).is_disabled:
-        return
     JudgeDispatcher(submission_id, problem_id).judge()
+
+
+@dramatiq.actor
+def my_task(arg1, arg2):
+    print(f"Processing task with {arg1} and {arg2}")
+    sleep(5)
+    print(f"Task with {arg1} and {arg2} completed")
