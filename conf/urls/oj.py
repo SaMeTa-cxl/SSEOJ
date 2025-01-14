a0bd4cf9ce1cf321cf7a84caf358cc32396721b9
SaMeTa-cxl <chenxlei23@mail2.sysu2.edu.cn>
2025-01-15 05:34:54 +0800

    修了一吨bugs

--- conf/urls/oj.py
+++ conf/urls/oj.py
@@ -1,8 +1,9 @@
 
 from django.urls import path
 
-from conf.views import JudgeServerHeartbeatAPI
+from conf.views import JudgeServerHeartbeatAPI, UploadImageAPI
 
 urlpatterns = [
     path("judge_server_heartbeat/", JudgeServerHeartbeatAPI.as_view(), name="judge_server_heartbeat_api"),
+    path("upload/", UploadImageAPI.as_view(), name="upload_image_api"),
 ]
